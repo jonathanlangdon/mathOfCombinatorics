9193ab85366ee961e31cccfdbc7695956c345e1f
Jonathan Langdon <langdon.jonathan@gmail.com>
2023-11-15 18:52:43 -0500

    Problem Set 1.1 How many integers are there from 25 to 79 inclusive?

---
+++ problem1-1.js
@@ -0,0 +1,7 @@
+// Problem Set 1.1 How many integers are there from 25 to 79 inclusive?
+
+function integersInRange(startNum, endNum) {
+  return endNum - startNum + 1
+}
+
+console.log(integersInRange(25, 79))
