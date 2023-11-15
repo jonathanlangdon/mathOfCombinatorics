28512ad4781838f20a024297fa9cb6c9d3703358
Jonathan Langdon <langdon.jonathan@gmail.com>
2023-11-14 22:43:57 -0500

    friday 13th - days of months

---
+++ friday_the_13.py
@@ -0,0 +1,16 @@
+# Determine how many Friday the 13th's are possible in a year
+
+num_days_in_month = {
+    "Jan": 31,
+    "Feb": 28,
+    "Mar": 31,
+    "Apr": 30,
+    "May": 31,
+    "Jun": 30,
+    "Jul": 31,
+    "Aug": 31,
+    "Sep": 30,
+    "Oct": 31,
+    "Nov": 30,
+    "Dec": 31,
+}
