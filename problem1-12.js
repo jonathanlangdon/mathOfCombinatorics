a99b32398c56b422a4a87993296b0b5038908cf2
Jonathan Langdon <langdon.jonathan@gmail.com>
2023-11-17 17:04:01 -0500

    // problem 1-12 A man has six pairs of cuff links scrambled in a box.

---
+++ problem1-12.js
@@ -0,0 +1,4 @@
+// problem 1-12 A man has six pairs of cuff links scrambled in a box.
+// No two pairs are alike. How many cuff links does he have to draw out all at once (in the dark) in order to be certain to get a pair that match?
+
+// answer: 7
