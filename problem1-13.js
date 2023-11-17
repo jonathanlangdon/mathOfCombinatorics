c60b229865df977a8097123e3eede719f3ee874f
Jonathan Langdon <langdon.jonathan@gmail.com>
2023-11-17 17:04:04 -0500

    // problem 1-13 A man has twelve blue socks and twelve black socks scrambled in a drawer.

---
+++ problem1-13.js
@@ -0,0 +1,4 @@
+// problem 1-13 A man has twelve blue socks and twelve black socks scrambled in a drawer.
+// How many socks does he have to draw out all at once (in the dark) to be certain to get a matching pair? (Any two blue or any two black socks, constitute a pair.)
+
+// answer: 3
